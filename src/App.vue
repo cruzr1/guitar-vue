48059328a3b24aeca66c7ddba8ff10a1b7fb01c4
Rinat <r.z.zaripov@gmail.com>
2024-03-06 11:42:02 +0300

    feat: добавил корневой компонент

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script lang="ts">
+import HeaderView from './components/HeaderView.vue';
+import FooterView from './components/FooterView.vue';
+import ErrorView from './components/ErrorView.vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent ({
+  name: 'App',
+  components: {
+    HeaderView,
+    FooterView,
+    ErrorView
+  },
+  async created() {
+    await this.$store.dispatch('authoriseUser');
+  }
+})
+</script>
+
+
+<template>
+  <ErrorView />
+  <div className="wrapper">
+    <HeaderView />
+    <main className="page-content">
+      <RouterView />
+    </main>
+    <FooterView />
+  </div>
+</template>
+
+<style lang="css">
+@import '../markup/css/style.css'
+</style>
